0a208fed54b87d4a07d30ca9bc6aba9aeea9733d
Team Eulogik <developers@eulogik.com>
2026-09-09 17:36:26 +0530

    chore: bump to v0.2.0 (v9 final adapters)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="polywhisper",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(),
     python_requires=">=3.9",
     install_requires=[
